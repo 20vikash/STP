eff7143cc8dd002e7f54a8a1e1d142c54404bee8
ssvikash_20 <arisivikash@gmail.com>
2025-09-07 16:46:00 +0530

    Connect interfaces

---
+++ src/internal/switch/connect.go
@@ -0,0 +1,10 @@
+package sw
+
+import "STP/STP/models"
+
+func ConnectInterface(sw1 *models.Switch, sw2 *models.Switch, type_ string, sPort int, dPort int) {
+	sw1.Interfaces[sPort].Pair = sw2
+	sw2.Interfaces[dPort].Pair = sw1
+
+	// Trigger STP operation once it is done
+}
--- src/models/switch.go
+++ src/models/switch.go
@@ -1,5 +1,7 @@
 package models
 
+import "STP/STP/consts"
+
 type Switch struct {
 	Id         int
 	Interfaces []*Interface
@@ -9,4 +11,36 @@ type Interface struct {
 	Type   string // FastEthernet and GigEthernet consts
 	Number int
 	Pair   *Switch
+}
+
+var ID = 0
+
+func CreateInterface(sw *Switch, type_ string) {
+	for i := range 3 {
+		interface_ := new(Interface)
+		interface_.Type = type_
+		interface_.Pair = nil
+		interface_.Number = i
+
+		sw.Interfaces = append(sw.Interfaces, interface_)
+	}
+}
+
+func CreateSwitch() *Switch {
+	// Creates a switch with fixed interfaces (3 Fast ethernets and 3 Gig Ethernets)
+	// Create the interfaces.
+
+	// Switch
+	sw := new(Switch)
+
+	// Fast ethernet
+	CreateInterface(sw, consts.FASTETHERNET)
+
+	// Gig Ethernet
+	CreateInterface(sw, consts.GIGETHERNET)
+
+	sw.Id = ID
+	ID += 1 // For the next switch
+
+	return sw
 }
